eac0bf1bc35a1492db8137b0a9918fb2a3ad2e8f
arjunKay <arjun.kay.5@mail.com>
2017-11-26 03:11:00 +0530

    Lab Reports activity and adapter implemented

---
+++ app/src/main/java/com/carehack/medivault/LabReport.java
@@ -0,0 +1,50 @@
+package com.carehack.medivault;
+
+import android.nfc.NfcAdapter;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+
+public class LabReport extends AppCompatActivity {
+
+    private FirebaseDatabase firebase;
+    private DatabaseReference ref;
+    private ArrayList<DataClass> labReports;
+    private RecyclerView labReportRV;
+    private RecyclerView.Adapter labReportsAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lab_report);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        labReports=new ArrayList<>();
+        firebase=FirebaseDatabase.getInstance();
+        ref=firebase.getReference().child("Lab Report");
+
+        labReports.add(new DataClass());
+        labReports.get(0).setTitle("Riverrun Test Clinic");
+        labReports.get(0).setDate("21-09-2017");
+
+        labReports.add(new DataClass());
+        labReports.get(1).setTitle("Aerys Clinic");
+        labReports.get(1).setDate("2-11-2017");
+
+        labReportRV=findViewById(R.id.labReportRV);
+        labReportRV.setLayoutManager(new LinearLayoutManager(LabReport.this));
+        labReportsAdapter=new LabReportsRVAdapter(labReports);
+        labReportRV.setAdapter(labReportsAdapter);
+    }
+
+}
---
+++ app/src/main/java/com/carehack/medivault/LabReportsRVAdapter.java
@@ -0,0 +1,51 @@
+package com.carehack.medivault;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.zip.Inflater;
+
+/**
+ * Created by user on 11/26/2017.
+ */
+
+public class LabReportsRVAdapter extends RecyclerView.Adapter<LabReportsRVAdapter.MyViewHolder> {
+
+    public LabReportsRVAdapter(ArrayList<DataClass> data) {
+        this.data = data;
+    }
+
+    ArrayList<DataClass> data;
+    @Override
+    public LabReportsRVAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v= LayoutInflater.from(parent.getContext()).inflate(R.layout.card_lab_view_report,parent, false);
+
+        return new MyViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(LabReportsRVAdapter.MyViewHolder holder, int position) {
+
+            holder.dateTV.setText(data.get(position).getDate());
+            holder.labNameTV.setText(data.get(position).getTitle());
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+
+        TextView labNameTV, dateTV;
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            labNameTV=itemView.findViewById(R.id.course_name);
+            dateTV=itemView.findViewById(R.id.duration);
+        }
+    }
+}
