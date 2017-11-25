c04cc39e2b1815f42b70b56ecee618809120311d
arjunKay <arjun.kay.5@mail.com>
2017-11-25 23:54:26 +0530

    Key generation and assignment encapsulated into SharedKeyManager

--- app/src/main/java/com/carehack/medivault/MainActivity.java
+++ app/src/main/java/com/carehack/medivault/MainActivity.java
@@ -8,21 +8,29 @@ import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
     enum Window{HOME, PENDING}
     BottomNavigationView bottomBar;
     FragmentManager fragmentManager;
+    private FirebaseDatabase firebase;
+    private DatabaseReference ref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        firebase=FirebaseDatabase.getInstance();
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Dashboard");
@@ -45,6 +53,27 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
+        final SharedPreferences sharedPreferences=getSharedPreferences(Utils.pref, MODE_PRIVATE);
+        ref=firebase.getReference();
+        ref.child("Shared Key").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String phone=sharedPreferences.getString("phone", null);
+
+                if(!dataSnapshot.hasChild(phone))
+                {
+                    Log.v(">>",dataSnapshot.toString());
+                    SharedKeyManager skm=new SharedKeyManager();
+                    skm.assignNewSharedKey(phone);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
     }
     private void showWindow(Window window) {
         switch (window){
---
+++ app/src/main/java/com/carehack/medivault/SharedKeyManager.java
@@ -0,0 +1,62 @@
+package com.carehack.medivault;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+/**
+ * Created by user on 11/25/2017.
+ */
+
+public class SharedKeyManager {
+    String phone;
+    String sharedKey;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+
+    public SharedKeyManager() {
+        this.firebaseDatabase=FirebaseDatabase.getInstance();
+        this.databaseReference=firebaseDatabase.getReference();
+    }
+
+
+    public void assignNewSharedKey(final String phone)
+    {
+        databaseReference.child("Shared Key").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String tempKey=Utils.randomSequenceGenerator(15);
+                while(dataSnapshot.hasChild(phone) && dataSnapshot.child(phone).getValue().toString().equals(tempKey))
+                {
+                    tempKey=Utils.randomSequenceGenerator(15);
+                }
+                databaseReference.child("Shared Key").child(phone).setValue(tempKey);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getSharedKey() {
+        return sharedKey;
+    }
+
+    public void setSharedKey(String sharedKey) {
+        this.sharedKey = sharedKey;
+    }
+
+
+}
