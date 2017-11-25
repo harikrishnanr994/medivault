63c1305bc8895d3cd4a9220b70a7eb8bc611a952
bineeshpbabu <bineesh@creopedia.com>
2017-11-25 14:51:42 +0530

    updated some

--- app/src/main/java/com/carehack/medivault/MainActivity.java
+++ app/src/main/java/com/carehack/medivault/MainActivity.java
@@ -1,13 +1,56 @@
 package com.carehack.medivault;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    enum Window{HOME, PENDING}
+    BottomNavigationView bottomBar;
+    FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Dashboard");
+        bottomBar = findViewById(R.id.bottomNavigationView);
+        fragmentManager=getSupportFragmentManager();
+        bottomBar.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if(bottomBar.getSelectedItemId() != item.getItemId()){
+                    if(item.getItemId() == R.id.navigation_menu_home)
+                        showWindow(Window.HOME);
+                    else if(item.getItemId() == R.id.navigation_menu_pending)
+                        showWindow(Window.PENDING);
+
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+    private void showWindow(Window window) {
+        switch (window){
+            case HOME:
+                fragmentManager.beginTransaction()
+                        .replace(R.id.dashboard_frag_lay, new HomeFragment())
+                        .commit();
+                break;
+            case PENDING:
+                fragmentManager.beginTransaction()
+                        .replace(R.id.dashboard_frag_lay, new PendingFragment())
+                        .commit();
+                break;
+
+        }
     }
 }
---
+++ app/src/main/java/com/carehack/medivault/PendingFragment.java
@@ -0,0 +1,19 @@
+package com.carehack.medivault;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Bineesh on 25/11/2017.
+ */
+
+public class PendingFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+}
